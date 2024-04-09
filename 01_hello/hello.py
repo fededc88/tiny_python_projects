d9fc882050f6796a9c3855434598c49622fe7623
Federico D. Ceccarelli <fededc88@gmail.com>
2024-04-09 00:09:41 -0400

    added call to main

--- 01_hello/hello.py
+++ 01_hello/hello.py
@@ -3,13 +3,17 @@
 
 import argparse
 
-parser = argparse.ArgumentParser(prog='Hello World', 
-                                 description='Say hello to my litle frient',
-                                 epilog='Text at the bottom of the help')
 
-parser.add_argument('-n', '--name', metavar='name',default='World', help='Name to greet')
+def main():
 
-args = parser.parse_args()
+    parser = argparse.ArgumentParser(prog='Hello World', 
+                                     description='Say hello to my litle frient',
+                                     epilog='Text at the bottom of the help')
+    parser.add_argument('-n', '--name', metavar='name',default='World', help='Name to greet')
+    
+    args = parser.parse_args()
+    
+    print('Hello, ' + args.name + '!')
 
-print('Hello, ' + args.name + '!')
-
+if __name__ == '__main__':
+    main()

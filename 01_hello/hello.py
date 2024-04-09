90c631fa466474b2c0b35192f9e6d7905112c92a
Federico D. Ceccarelli <fededc88@gmail.com>
2024-04-08 23:53:24 -0400

    added positional arguments

--- 01_hello/hello.py
+++ 01_hello/hello.py
@@ -1,1 +1,12 @@
-print('Hello, World!')
+#!/usr/bin/env python3
+# Purpose: Say Hello
+
+import argparse
+
+parser = argparse.ArgumentParser(description='Say hello')
+parser.add_argument('name', help='Name to greet')
+
+args = parser.parse_args()
+
+print('Hello, ' + args.name + '!')
+

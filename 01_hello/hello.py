bb29ebc326d33f374a7d2d4bf8e6ac5858b5292e
Federico D. Ceccarelli <fededc88@gmail.com>
2024-04-09 00:11:48 -0400

    defined and implemented get_args()

--- 01_hello/hello.py
+++ 01_hello/hello.py
@@ -3,15 +3,19 @@
 
 import argparse
 
-
-def main():
-
+def get_args():
     parser = argparse.ArgumentParser(prog='Hello World', 
                                      description='Say hello to my litle frient',
                                      epilog='Text at the bottom of the help')
     parser.add_argument('-n', '--name', metavar='name',default='World', help='Name to greet')
     
-    args = parser.parse_args()
+    return parser.parse_args()
+
+
+
+def main():
+
+    args = get_args()
     
     print('Hello, ' + args.name + '!')
 

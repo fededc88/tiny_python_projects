073ce24ec74caf921f3867bc82a81ffd9cc7f886
Federico D. Ceccarelli <fededc88@gmail.com>
2024-04-11 01:22:26 -0400

    solution chapter 2:

---
+++ 02_crowsnest/crowsnest.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+"""
+Author : fededc88 <fededc88@localhost>
+Date   : 2024-04-10
+Purpose: Rock the Casbah
+"""
+
+import argparse
+
+
+# --------------------------------------------------
+def get_args():
+    """Get command-line arguments"""
+
+    parser = argparse.ArgumentParser(
+        description='Crow\'s nest -- chose the correct article',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    parser.add_argument('word',
+                        metavar='word',
+                        help='a word')
+
+    return parser.parse_args()
+
+
+# --------------------------------------------------
+def main():
+    """Make a jazz noise here"""
+
+    args = get_args()
+    word = args.word
+
+    article = 'an' if word[0].lower() in 'aeiou' else 'a'
+
+    print(f'Ahoy, Captain, {article} {word} off the larboard bow!')
+
+
+# --------------------------------------------------
+if __name__ == '__main__':
+    main()

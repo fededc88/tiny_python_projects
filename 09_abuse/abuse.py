80c9cd5672ed8cc3bdeb02317c15c8c3831b8793
Federico D. Ceccarelli <fededc88@gmail.com>
2024-05-08 21:42:39 -0400

    chapter 09

--- 09_abuse/abuse.py
+++ 09_abuse/abuse.py
@@ -3,6 +3,21 @@
 import argparse
 import random
 
+adj = """bankrupt base caterwauling corrupt cullionly detestable dishonest false
+filthsome filthy foolish foul gross heedless indistinguishable infected insatiate
+irksome lascivious lecherous loathsome lubbery old peevish rsacaly rotten
+ruinous scurilous scurvy slanderous sodden-witted thin-faced toad-spotted
+unmannered vile wall-eyed""".strip().split()
+
+assert len(adj) == 36
+
+nouns = """Judas Satan ape ass barbermonger beggar blsck boy braggart butt
+carbuncle coward coxcomb cur dandy degenerate fiend fishmonger fool gull harpy
+jack jolthead knave liar lunatic maw milsop minion ratcatcher recreant togue
+scold slave swine traitor varlet villain worm""".strip().split()
+
+assert len(nouns) == 39
+
 def get_args():
     
     parser = argparse.ArgumentParser(
@@ -38,12 +53,15 @@ def get_args():
     return args
 
 def main():
+
+
     args = get_args()
 
     random.seed(args.seed)
 
-    print(random.randint(0,1000))
-
+    for i in range(args.number):
+        #print('You {} {}!'.format(', '.join(random.choices(adj, k = args.adjectives)), random.choice(nouns))) 
+        print('You ' + ', '.join(random.sample(adj, k = args.adjectives)) + ' ' + random.choice(nouns) + '!') 
 
 if __name__ == '__main__':
     main()
